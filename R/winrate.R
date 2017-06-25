be59dc9beb28c0cb427d530f962d009d7e7bb787
Sergii Kozak <sergii.kozak@outlook.com>
2017-06-25 18:56:01 +0200

    calculate winrate

--- R/winrate.R
+++ R/winrate.R
@@ -3,8 +3,30 @@ winrate <- function(){
 
   games <- fromJSON("http://foosball-results.herokuapp.com/api/games", flatten = TRUE)
 
-  games <- games[games$blue.score != games$red.score]
+  games <- games[games$blue.score != games$red.score,]
 
-  return(games)
+  blueWiners <- games[games$blue.score > games$red.score,]
+  redWinners <- games[games$red.score > games$blue.score,]
+
+  blueWiners <- data.frame(offense = blueWiners$blue.offense._id, defense = blueWiners$blue.defense._id)
+  redWinners <- data.frame(offense = redWinners$red.offense._id, defense = redWinners$red.defense._id)
+
+  winners <- rbind(blueWiners, redWinners)
+
+  offense <- winners$offense
+  defense <- winners$defense
+
+  offense.relfreq = table(offense)/nrow(winners)
+  defense.relfreq = table(defense)/nrow(winners)
+
+  result <- cbind(offense.relfreq, defense.relfreq)
+
+  result <- data.frame(result)
+
+  result <- data.frame(Id = rownames(result), offenseWinRate = result$offense.relfreq, defenseWinRate = result$defense.relfreq)
+
+  return(toJSON(result))
 
 }
+
+print(winrate())
