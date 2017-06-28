74d69885f3e8e544d13b26c65f49024cb149ad7b
Sergii Kozak <sergii.kozak@outlook.com>
2017-06-29 01:56:22 +0200

    Exclude duplicates

--- R/winrate.R
+++ R/winrate.R
@@ -1,9 +1,18 @@
 winrate <- function(d){
   library(jsonlite)
+  library(data.table)
 
   games <- fromJSON("http://foosball-results.herokuapp.com/api/games", flatten = TRUE)
 
   games <- games[games$blue.score != games$red.score,]
+
+  dupes <- merge(games, games, by.x = c("blue.offense._id", "blue.defense._id", "red.offense._id", "red.defense._id", "blue.score", "red.score"), by.y = c("blue.offense._id", "blue.defense._id", "red.offense._id", "red.defense._id", "blue.score", "red.score"))
+  dupes <- dupes[dupes$`_id.x` != dupes$`_id.y`,]
+  dupes <- dupes[abs(difftime(strptime(dupes$endDate.x, format = '%Y-%m-%dT%H:%M:%OSZ'), strptime(dupes$endDate.y, format = '%Y-%m-%dT%H:%M:%OSZ'), units = c("mins"))) < 10,]
+  dupes <- dupes[grep("^DrumStats", dupes$source.x),]
+
+  games <- subset(games, !(games$`_id` %in% dupes$`_id.y`))
+
 
   blueWiners <- games[games$blue.score > games$red.score,]
   redWinners <- games[games$red.score > games$blue.score,]
